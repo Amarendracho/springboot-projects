2d7fddc47c28b5e1ff1f514ff62a796499ba1829
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-10 14:54:52 -0400

    update springwothoutboot

--- springwithoutboot/src/main/java/com/amar/springwithoutboot/App.java
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/App.java
@@ -4,6 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("SPRING WITHOUT BOOT");
+       
+    	Hospital h = new Hospital();
+    	h.patientDepartment();
     }
 }
---
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/Hospital.java
@@ -0,0 +1,9 @@
+package com.amar.springwithoutboot;
+
+public class Hospital {
+	
+	public void patientDepartment() {
+		System.out.println("This Patient is this department ....");
+	}
+
+}
