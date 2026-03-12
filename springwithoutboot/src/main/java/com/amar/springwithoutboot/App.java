9f5364c513ee68535e4afa40cd4b44c1263e4c56
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-12 18:41:58 -0400

    updtae springwithoutboot project

--- springwithoutboot/src/main/java/com/amar/springwithoutboot/App.java
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/App.java
@@ -12,7 +12,7 @@ public class App
     	Hospital h = (Hospital) context.getBean("hospital");
     	
     	ChildranHospital c = (ChildranHospital) context.getBean("childran");
-    	System.out.println();
+    	c.getPatientName();
     	h.patientDepartment();
     	
     }
