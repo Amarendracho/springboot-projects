6dea40aa5faccdb4260df9ff43f3f7f1ae10e726
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-12 18:50:39 -0400

    updtae springwithoutboot project

--- springwithoutboot/src/main/java/com/amar/springwithoutboot/ChildranHospital.java
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/ChildranHospital.java
@@ -11,7 +11,7 @@ public class ChildranHospital {
 	//CONSTRUCTOR INJECTION
 	public ChildranHospital(String parientName) {
 		this.patientName = parientName;
-		System.out.println("CHILDRAN HOSPITAL PARAMETERIZED ");
+		System.out.println("CHILDRAN HOSPITAL PARAMETERIZED CONSTRUCTION");
 	}
 	
 	//GETTERS AND SETTERS 
