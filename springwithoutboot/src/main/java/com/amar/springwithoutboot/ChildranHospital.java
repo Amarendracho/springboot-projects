957d9981e89b0f621929035cc4e584e5f92b9a57
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-12 18:36:02 -0400

    updtae springwithoutboot project

--- springwithoutboot/src/main/java/com/amar/springwithoutboot/ChildranHospital.java
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/ChildranHospital.java
@@ -4,10 +4,10 @@ public class ChildranHospital {
 	
 	private String parientName = "Mark Sloan";
 	
-	//CONSTRUCTOR INJECTION
-	
 	public ChildranHospital() {
 		System.out.println("CHILDRAN HOSPITAL");
 	}
+	
+	//CONSTRUCTOR INJECTION
 
 } 
