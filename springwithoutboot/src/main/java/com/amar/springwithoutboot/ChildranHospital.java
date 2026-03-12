33b7ae92d3d1167cd581c373885650cfca61b67d
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-12 18:33:28 -0400

    updtae springwithoutboot project

--- springwithoutboot/src/main/java/com/amar/springwithoutboot/ChildranHospital.java
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/ChildranHospital.java
@@ -2,7 +2,7 @@ package com.amar.springwithoutboot;
 
 public class ChildranHospital {
 	
-	String parientName;
+	String parientName = "Mark Sloan";
 	
 	public ChildranHospital() {
 		System.out.println("CHILDRAN HOSPITAL");
