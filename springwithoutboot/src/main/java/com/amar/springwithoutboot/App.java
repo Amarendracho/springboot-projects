b2a05f56b4b3d69e36ad1bd0c526cefe147716e6
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-13 10:33:17 -0400

    update springwithoutboot project

--- springwithoutboot/src/main/java/com/amar/springwithoutboot/App.java
+++ springwithoutboot/src/main/java/com/amar/springwithoutboot/App.java
@@ -3,20 +3,18 @@ package com.amar.springwithoutboot;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	
-    	@SuppressWarnings("resource")
+public class App {
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("resources/spring.xml");
-    	//Hospital h = (Hospital) context.getBean("hospital");
-    	
-    	ChildranHospital c = (ChildranHospital) context.getBean("childran");
-    	//System.out.println(c.getPatientName());
-    	//h.patientDepartment();
-    	
-    	
-    }
-} 
-  
+		// Hospital h = (Hospital) context.getBean("hospital");
+
+		ChildranHospital c = (ChildranHospital) context.getBean("childran");
+		c.setPatientName("JORDON");
+		c.setPatientAge(25);
+		System.out.println(c.getPatientName());
+		System.out.println(c.getPatientAge());
+
+	}
+}
